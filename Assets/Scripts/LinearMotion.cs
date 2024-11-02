07f190d2bc7228b601814feaf776e5caadaa798d
sikk806 <sikk806@gmail.com>
2024-11-02 16:11:02 +0900

    apple

--- Assets/Scripts/LinearMotion.cs
+++ Assets/Scripts/LinearMotion.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class LinearMotion : MonoBehaviour
 {
+    [SerializeField]
+    float Vy = 0;
+    [SerializeField]
+    bool isTrigger = false;
+
+    bool bEnter = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (bEnter)
+        {
+            GetComponent<Rigidbody2D>().linearVelocityY = Vy;
+            bEnter = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player" && isTrigger)
+        {
+            bEnter = true;
+        }
     }
 }
